0efdc100c38cc8f160cc14ce667c3ce82bc2fb10
Isaias00C <167316633+Isaias00C@users.noreply.github.com>
2024-05-29 20:19:34 +0000

    questoes

--- maratona-fase0/QM.cpp
+++ maratona-fase0/QM.cpp
@@ -3,4 +3,15 @@ using namespace std;
 
 int main(){
     int num_casas, num_estradas, distancia;
+    vector<vector<int>> grafo;
+
+    for(int i = 0; i < num_estradas; i++){
+        int casa1, casa2;
+        cin >> casa1 >> casa2;
+        grafo[casa1].push_back(casa2);
+        grafo[casa2].push_back(casa1);
+    }
+
+    queue<int,vector<int>> fila;
+
 }
