12ada6d895f6df34fffe753100ee3fab26dc9ef6
Isaias00C <167316633+Isaias00C@users.noreply.github.com>
2024-05-31 15:03:43 +0000

    questoes

--- semana6/NEPS-analise-de-texto.cpp
+++ semana6/NEPS-analise-de-texto.cpp
@@ -2,23 +2,27 @@
 using namespace std;
 
 int main(){
-    string frase, controle;
-    getline(cin, frase);
-    cin >> controle;
+    static const basic_string <char>::size_type npos = -1;
+    vector<string> frase;
+    string palavra, controle;
+    while(getline(cin, palavra, ' ')){
+        frase.push_back(palavra);
+    }
     int contador_de_palavras = 0;
 
+    string aux = frase[frase.size() - 1], aux2;
+
+    int flag = aux.find_first_of("\n");
+    //find_first_of(<string>) -> retorna o indice da primeira ocorrencia de um dos caracteres da <string>
+    controle = aux.substr(flag + 1, aux.size() - flag + 1);
+    aux2 = aux.substr(0, flag);
+    //substr -> copia a substring a partir de um indice especificado com um numero de caracteres especificado
+    frase.pop_back();
+    frase.push_back(aux2);
     for(int i = 0; i < frase.size(); i++){
-        for(int j = 0; j < controle.size(); j++){
-            if(frase[i] == controle[j]){
-                contador_de_palavras++;
-                while(i < frase.size()){
-                    if(frase[i] == ' ') break;
-                    i++;
-                }
-                break;
-            }
+        if(frase[i].find_first_of(controle) != npos){
+            contador_de_palavras++;
         }
     }
-
     cout << contador_de_palavras;
 }
--- semana6/NEPS-colheita.cpp
+++ semana6/NEPS-colheita.cpp
@@ -13,8 +13,21 @@ int main(){
     }
 
     int maior_valor = 0;
-    for(int i = 0, j = 0; i < colunas - 1 && j < linhas - 1; i++){
-        
+    for(int m = 0; m < linhas - 1; m++){
+        for(int n = 0; n < colunas - 1; n++){
+            //rodar toda a linha    
+            for(int j = m; j < colunas; j++){    
+                if(matriz[n][m] - matriz[n][j] > maior_valor ){
+                    maior_valor = matriz[n][m] - matriz[n][j];
+                }
+            }
+            //rodar toda a coluna
+            for(int i = n; i < linhas; i++){
+                if(matriz[n][m] - matriz[i][m] > maior_valor){
+                    maior_valor = matriz[n][m] - matriz[i][m];
+                }
+            }
+        }
     }
     cout << maior_valor;
 }
