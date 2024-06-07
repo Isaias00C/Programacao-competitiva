44848d2f27e160b79394982782aca610490aface
Isaias00C <167316633+Isaias00C@users.noreply.github.com>
2024-06-07 22:21:20 +0000

    questoes

--- semana7/LEETCODE-flood-fill.cpp
+++ semana7/LEETCODE-flood-fill.cpp
@@ -3,27 +3,41 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         queue<pair<int,int>> queue;
         queue.push({sr,sc});
-        image[sr][sc] = color;
+        set<pair<int,int>> visitadas;
         while(!queue.empty()){
             int row = queue.front().first;
             int column = queue.front().second;
             queue.pop();
-            if(image[row+1][column] == 1){
-                queue.push({row+1,column});
-                image[row+1][column] = color;
+            if(row+1 < image.size()){    
+                if(image[row+1][column] == image[row][column]){
+                    if(visitadas.find({row+1,column}) == visitadas.end()){    
+                        queue.push({row+1,column});
+                    }
+                }
             }
-            if(image[row-1][column] == 1){
-                queue.push({row-1,column});
-                image[row-1][column] = color;
+            if(row-1 >= 0){    
+                if(image[row-1][column] == image[row][column]){
+                    if(visitadas.find({row-1,column}) == visitadas.end()){    
+                        queue.push({row-1,column});
+                    }
+                }
             }
-            if(image[row][column+1] == 1){
-                queue.push({row,column+1});
-                image[row][column+1] = color;
+            if(column+1 < image[0].size()){
+                if(image[row][column+1] == image[row][column]){
+                    if(visitadas.find({row,column+1}) == visitadas.end()){    
+                        queue.push({row,column+1});
+                    }
+                }
             }
-            if(image[row][column-1] == 1){
-                queue.push({row,column-1});
-                image[row][column-1] = color;
+            if(column-1 >= 0){
+                if(image[row][column-1] == image[row][column]){
+                    if(visitadas.find({row,column-1}) == visitadas.end()){    
+                        queue.push({row,column-1});
+                    }
+                }
             }
+            visitadas.insert({row,column});
+            image[row][column] = color;
         }
         return image;
     }
---
+++ semana7/LEETCODE-max-area-of-island.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int max = 0;
+        queue<int> queue;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                int row = i;
+                int column = i;
+                
+            }
+        }
+    }
+};
