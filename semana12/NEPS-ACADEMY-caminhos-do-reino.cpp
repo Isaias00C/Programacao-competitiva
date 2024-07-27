045a75cf37fd9217f1478b2cca759dfd42509762
Isaias00C <167316633+Isaias00C@users.noreply.github.com>
2024-07-27 10:40:01 -0300

    falta condicao de chegada na parte ciclica do reino

--- semana12/NEPS-ACADEMY-caminhos-do-reino.cpp
+++ semana12/NEPS-ACADEMY-caminhos-do-reino.cpp
@@ -4,4 +4,29 @@ using namespace std;
 int main(){
     int numCities;
     cin >> numCities;
+    vector<int> to;
+
+    for(int i = 0; i < numCities; i++){
+        int city;
+        cin >> city;
+        to.push_back(city); //to[i] é o destino da cidade i
+    }
+
+    int quests;
+    cin >> quests;
+
+    while(quests--){
+        int city1, city2, ans = 0;
+        cin >> city1 >> city2;
+        while(1){
+            if(to[city1] == city2 || to[city2] == city1){
+                cout << ans + 1 << "\n";
+                break;
+            }
+            city1 = to[city1];
+            city2 = to[city2];
+            
+        }
+    }
+
 }
