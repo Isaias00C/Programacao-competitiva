4f68d79ac3d940d7b69650241620f2a6b74eb475
Isaias Castro <isaiascastro2014@gmail.com>
2024-08-14 11:20:44 -0300

    update

--- diversas/NEPSACADEMY/cartomancia.cpp
+++ diversas/NEPSACADEMY/cartomancia.cpp
@@ -17,20 +17,28 @@ void f(){
 
     cin >> enzo >> lobo >> gab;
 
-    int acertos_enzo = 0, acertos_lobo = 0;
+    int acertos_enzo = 0, acertos_lobo = 0, margem_de_erro = 0;
     for(int i = 0; gab[i] != '\0'; i++){
         if(gab[i] != '?'){    
             if(enzo[i] == gab[i] || enzo[i] == '?') acertos_enzo++;
             if(lobo[i] == gab[i] || lobo[i] == '?') acertos_lobo++;
         }else {
-            if(enzo[i] == lobo[i]){
-
-            }else{
-
+            if(enzo[i] != lobo[i]){
+                margem_de_erro++;
             }
         }
     }
 
-    if(acertos_enzo > acertos_lobo) cout << "Enzo";
-    else if(acertos_lobo > acertos_enzo) cout << "Lobo";
+    cout << "Acertos do Enzo: " << acertos_enzo << "\n";
+    cout << "Acertos do Lobo: " << acertos_lobo << "\n";
+    cout << "Margem de Erro: " << margem_de_erro << "\n";
+
+    if(max(acertos_enzo, acertos_lobo) - min(acertos_enzo, acertos_lobo) > margem_de_erro){
+        if(acertos_enzo > acertos_lobo) cout << "Enzo";
+        else cout << "Lobo";
+    }else {
+        
+        cout << "Indefinido";
+    }
 }
+
