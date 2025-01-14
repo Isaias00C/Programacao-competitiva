e079d15023af8a645edff2a7e99256c4bc70a2a2
Isaias Castro <isaiascastro2014@gmail.com>
2025-01-14 10:32:37 -0300

    commit

---
+++ algoritmos/ford-fulkerson.cpp
@@ -0,0 +1,22 @@
+#include <btis/stdc++.h>
+using namespace std;
+
+vector<int> bfs(vector<vector<int>> G, int s, int t);
+
+int main(){
+
+}
+
+vector<int> bfs(vector<vector<pair<int,int>>> G, int s, int t){
+    vector<int> vis;
+    queue<int> q;
+    q.push(s);
+
+    while (!q.empty()){
+        int currNode = q.front();
+        q.pop();
+        
+        
+    }
+    
+}   
