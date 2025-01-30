94a9dc3451415eaf562fdea1b8ebed78b2e9a9a7
Isaias Castro <isaiascastro2014@gmail.com>
2025-01-30 10:42:04 -0300

    nao sei onde ta o erro

---
+++ diversas/NEPSACADEMY/Pizza.cpp
@@ -0,0 +1,38 @@
+//https://neps.academy/br/exercise/299
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+signed main(){
+    int qtd_de_fatias;
+    cin >> qtd_de_fatias;
+
+    vector<int> saldo (2*qtd_de_fatias);
+
+    for(int i = 0; i < qtd_de_fatias; ++i){
+        cin >> saldo[i];
+        saldo[qtd_de_fatias+i] = saldo[i];
+    }
+
+    int resp = saldo[0], aux = saldo[0];
+    int fatia_de_inicio = 0;
+
+    for(int i = 1; i < 2*qtd_de_fatias && i-fatia_de_inicio < qtd_de_fatias; ++i){
+        if (aux <= 0) {
+            aux = 0;
+            fatia_de_inicio = i;
+        }
+
+        aux += saldo[i];
+
+        if (aux > resp) resp = aux;
+    }
+    
+    resp = resp > 0 ? resp : 0;
+
+    cout << resp;
+
+    return 0;
+}
