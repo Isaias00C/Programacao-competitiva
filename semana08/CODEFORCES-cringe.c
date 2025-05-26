835189da11cf6fd9e15789ea53a083fadb7e9079
Isaias Castro <isaiascastro2014@gmail.com>
2025-05-26 07:43:11 -0300

    Deleted Files

--- semana08/CODEFORCES-cringe.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int year;
-    scanf("%d", &year);
-    printf("%d\n", 2024 - 2*(2024 - year));
-}
--- semana10/LEETCODE-fibonacci.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int fib(int n) {
-        int ans = (pow((1+sqrt(5.0))/double(2),n) - pow((1-sqrt(5.0))/double(2),n))/sqrt(5);
-        return ans;
-    }
-};
