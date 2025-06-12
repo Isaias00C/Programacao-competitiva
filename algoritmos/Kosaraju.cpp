3ba61ce2294bd893ef2602c9448525b0499fda84
Isaias Castro <isaiascastro2014@gmai.com>
2025-06-12 17:43:00 -0300

    Algoritmo de Kosaraju correto

--- algoritmos/Kosaraju.cpp
+++ algoritmos/Kosaraju.cpp
@@ -4,10 +4,7 @@ using namespace std;
 vector<vector<int>> GReverse(vector<vector<int>>& G){
     vector<vector<int>> _G (G.size());
     
-    // para cada vertice olharemos o destino dos arcos que saem dele
     for(int i = 0; i < G.size(); ++i){
-
-        // para cada vertice destino setaremos no grafo _G no vertice G[i][j] como sendo destino do vertice i
         for(int j = 0; j < G[i].size(); ++j){
             _G[G[i][j]].push_back(i);
         }
@@ -16,56 +13,41 @@ vector<vector<int>> GReverse(vector<vector<int>>& G){
     return _G;
 }
 
-vector<int> DFS(vector<vector<int>>& _G){
-    stack<int> s;
-    set<int> vis, inStack;
-    vector<int> order;
-
-    for(int i = 0; i < _G.size(); ++i){
-        if(vis.find(i) == vis.end()){
-            s.push(i);
-            
-            while(!s.empty()){
-                int currVertice = s.top();
-                s.pop();
-                
-                vis.insert(currVertice);
-                int hasChild = 0;
-                for(int v : _G[currVertice]){
-                    if(inStack.find(v) == inStack.end() && vis.find(v) == vis.end()){    
-                        s.push(v);
-                        hasChild++;
-                    }
-                }
-
-                if(!hasChild) order.push_back(currVertice);
-            }
+void DFS_PosOrder(int u, vector<vector<int>>& _G, vector<bool>& vis, vector<int>& order) {
+    vis[u] = true;
+    
+    for (int viz : _G[u]) {
+        if (!vis[viz]) {
+            DFS_PosOrder(viz, _G, vis, order);
         }
     }
-    return order;
-}
 
-int Counter(vector<vector<int>>& G, vector<int>& order){
+    order.push_back(u);
+}   
+
+int Counter(vector<vector<int>>& G, vector<int>& stackAux){
     set<int> vis;
     int count = 0;
 
-    for(int v : order){
+    for(int i = stackAux.size() - 1; i >= 0; --i){
+        int v = stackAux[i];
         if(vis.find(v) == vis.end()) {
+            count++;
             stack<int> s;
             s.push(v);
             vis.insert(v);
-
+            
             while(!s.empty()){
                 int currVertice = s.top();
                 s.pop();
                 
                 for(int adj : G[currVertice]){
-                    vis.insert(adj);
-                    s.push(adj);
+                    if(vis.find(adj) == vis.end()){
+                        s.push(adj);
+                        vis.insert(adj);
+                    } 
                 }
             }
-            
-            count++;
         }
     }
 
@@ -82,14 +64,22 @@ int main(){
         int a, b;
         cin >> a >> b;
 
-        G[a].push_back(b);
+        G[a-1].push_back(b-1);
     }
 
     vector<vector<int>> _G = GReverse(G);
 
-    vector<int> order = DFS(_G);
+    vector<bool> vis(V, false);
+    vector<int> s;
 
-    cout << Counter(G, order) << "\n";    
+    for(int i = 0; i < V; ++i){
+        if(!vis[i]) DFS_PosOrder(i, _G, vis, s);
+    }
+
+    int count = Counter(G, s);
+
+    cout << count << "\n";
+    cout << ((count == 1) ? "Yes\n" : "No\n");
 
     return 0;
 }
